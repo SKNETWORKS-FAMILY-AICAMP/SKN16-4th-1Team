ae365330452bd2b6e975f7e5db06c4c70bfaebb6
신지윤 <yoyooyooon0311@gmail.com>
2025-10-17 17:07:11 +0900

    메인화면 및 사이드바 수정

--- entry/urls.py
+++ entry/urls.py
@@ -3,8 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.entry, name='entry'),
+    # path('', views.login_view, name='home'),
     path('show', views.show, name='show'),
     path('show/<int:diary_id>', views.detail, name='detail'),
     path('productivity/', views.productivity, name='productivity'),
     path('generate-image/<int:diary_id>/', views.generate_image, name='generate_image'),
+    
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('accounts/signup/', views.signup_view, name='signup'),
+    path('accounts/profile/', views.profile_view, name='profile'),
+    path('accounts/settings/', views.settings_view, name='settings'),
 ]
